2474431d4f1125157a0e48ac486deb18f5bc76f8
RenanCalderon <renan.cnv@gmail.com>
2023-08-23 23:14:57 -0400

    Work in progress

--- run_music_suite.sh
+++ run_music_suite.sh
@@ -4,12 +4,10 @@ export SHELLOPTS
 set -o igncr
 
 APP_TYPE="$1"
-PATH="/c/Users/renan/Documents/Python/MusicSuiteApp/"
+PATH="/c/Users/renan/Documents/Python/App/"
 
 if [ -z "$1" ]; then
   echo "Run Music Suite Script"
-  export environment=$APP_TYPE
-  echo "Environment: "$environment""
   python ./music_suite_app.py --log-cli-level='INFO' -vv
 
 elif [ "$1" = "test" ]; then
