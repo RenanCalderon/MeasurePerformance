308df91b9e176a9e47f0701d2056a5bdfa5e07c6
RenanCalderon <renan.cnv@gmail.com>
2023-08-24 23:00:51 -0400

    Work in progress: Finish app environment

--- run_music_suite.sh
+++ run_music_suite.sh
@@ -1,10 +1,6 @@
 #! /usr/bin/env bash
 
-export SHELLOPTS
-set -o igncr
-
 APP_TYPE="$1"
-PATH="/c/Users/renan/Documents/Python/App/"
 
 if [ -z "$1" ]; then
   echo "Run Music Suite Script"
@@ -14,29 +10,33 @@ elif [ "$1" = "test" ]; then
   echo "Run Music Suite Test"
   export environment=$APP_TYPE
   echo "Environment: $environment"
-  FILE="$PATH""MusicTest/MusicTest_20230822/dist/MusicTest.exe"
-  echo "$FILE"
-  if [ ! -x "$FILE" ]; then
-      echo "The file does not have execute permission"
-
-
-      chmod +x "$FILE"
-      if [ $? -eq 0 ]; then
-          echo "Execute permission granted to the file."
-      else
-          echo "Failed to grant execute permission to the file."
-          exit 1
-      fi
-  fi
-
+  TEST="/c/Users/renan/Documents/Python/App/MusicTest/"
+  LATEST=$(ls -1 -t $TEST | head -n 1)
+  echo "$LATEST"
+  FILE="$TEST""$LATEST"/dist/MusicTest.exe
   "$FILE"
-
-elif [ "$APP_TYPE" = "app" ]; then
-  echo "Run Music Suite App"
-  export environment=$APP_TYPE
-  echo "Environment: $environment"
-else
-  echo "Bad Request"
 fi
+#  if [ ! -x "$FILE" ]; then
+#      echo "The file does not have execute permission"
+#
+#
+#      chmod +x "$FILE"
+#      if [ $? -eq 0 ]; then
+#          echo "Execute permission granted to the file."
+#      else
+#          echo "Failed to grant execute permission to the file."
+#          exit 1
+#      fi
+#  fi
+#
+#  "$FILE"
+#
+#elif [ "$APP_TYPE" = "app" ]; then
+#  echo "Run Music Suite App"
+#  export environment=$APP_TYPE
+#  echo "Environment: $environment"
+#else
+#  echo "Bad Request"
+#fi
 
 
