7f50959d2439feb0a7dfbedeed041d8c11883e5b
RenanCalderon <renan.cnv@gmail.com>
2023-08-26 11:09:42 -0400

    Update .gitignore

--- run_music_suite.sh
+++ run_music_suite.sh
@@ -1,9 +1,31 @@
 #! /usr/bin/env bash
 
-export SHELLOPTS
-set -o igncr
+APP_TYPE="$1"
 
-export environment="test"
-echo "Environment: $environment"
+if [ -z "$1" ]; then
+  echo "Run Music Suite Script"
+  python ./music_suite_app.py --log-cli-level='INFO' -vv
 
-python ./music_suite_app.py --log-cli-level='INFO' -vv
+elif [ "$1" = "test" ]; then
+  echo "Run Music Suite Test"
+  export environment=$APP_TYPE
+  echo "Environment: $environment"
+  TEST="/c/Users/renan/Documents/Python/App/MusicTest/"
+  LATEST=$(ls -1 -t $TEST | head -n 1)
+  echo "$LATEST"
+  FILE="$TEST""$LATEST"/dist/MusicTest.exe
+  "$FILE"
+
+elif [ "$1" = "app" ]; then
+  echo "Run Music Suite App"
+  export environment=$APP_TYPE
+  echo "Environment: $environment"
+  TEST="/c/Users/renan/Documents/Python/App/MusicSuite/"
+  LATEST=$(ls -1 -t $TEST | head -n 1)
+  echo "$LATEST"
+  FILE="$TEST""$LATEST"/dist/MusicSuite.exe
+  "$FILE"
+
+else
+  echo "Not valid input"
+fi
