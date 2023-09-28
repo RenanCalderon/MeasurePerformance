b1f6683c39fbdd59d06be9d18e0ff0f410f7443c
RenanCalderon <renan.cnv@gmail.com>
2023-09-27 22:22:38 -0400

    Query for create music tables

---
+++ db/music_tables.py
@@ -0,0 +1,72 @@
+import logging
+from utilities_db import create_connection, create_database
+from config import config
+
+LOG = logging.getLogger()
+
+
+def create_music_tables(environment):
+    if not environment:
+        print("Please enter the database name: ")
+        return
+
+    database_name = "music_" + environment
+
+    try:
+
+        # Create database
+        create_database(host, user, password, database_name)
+
+        # Connect to the database
+        connection = create_connection(host, user, password, database_name)
+
+        # Create Expenses Dimension Table
+        create_songs_table_query = """
+            CREATE TABLE IF NOT EXISTS songs (
+                id VARCHAR(255) PRIMARY KEY,
+                song_order INT,
+                name VARCHAR(255),
+                artist VARCHAR(255),
+                remix VARCHAR(255) NULL,
+                genre VARCHAR(255),
+                key_song VARCHAR(5),
+                camelot VARCHAR(3),
+                bpm INT,
+                label VARCHAR(255),
+                duration TIME,
+                remixer VARCHAR(255) NULL,
+                album VARCHAR(255),
+                rating VARCHAR(5) NULL,
+                date_added DATE
+            )
+        """
+        with connection.cursor() as cursor:
+            cursor.execute(create_songs_table_query)
+
+        # Create Expenses Dimension Table
+        create_sets_table_query = """
+                CREATE TABLE IF NOT EXISTS sets (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    name VARCHAR(255),
+                    bpm_range VARCHAR(10),
+                    date DATE,
+                    songs JSON NOT NULL
+                )
+            """
+
+        with connection.cursor() as cursor:
+            cursor.execute(create_sets_table_query)
+
+        connection.close()
+        print("Tables and procedures created successfully.")
+    except Exception as e:
+        LOG.error("An error occurred:", exc_info=True)
+        print("Error creating tables and procedures:", str(e))
+
+
+if __name__ == "__main__":
+    host = config["database"]["mysql_host"]
+    user = config["database"]["mysql_user"]
+    password = config["database"]["mysql_password"]
+    environment = input("Enter the environment name: ")
+    create_music_tables(environment)
